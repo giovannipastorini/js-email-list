37586b22ed0988808c1020e72c566ab0b1fcf6b8
giovannipastorini <giovannipastorini9@gmail.com>
2026-04-01 22:11:12 +0200

    commit

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 console.log("it works");
+
+
+const ulEl=document.getElementById("email-list");
+/* console.log(ulEl); */
+
+const api_endpoint='https://flynn.boolean.careers/exercises/api/random/mail';
+
+//utilizzo il metodo nativo ajax: fetch  
+for (let i=0; i<10; i++){
+   fetch(api_endpoint)
+  .then(response => response.json())
+  .then(data => {
+  	// the API response data isi inside the parameter
+  	console.log(data);
+    const liEl= document.createElement("li");
+    liEl.innerHTML=data.response;
+    ulEl.appendChild(liEl);
+  }) 
+}
+
