f776edecd56670d446dd86b51876ad2ba6eb310e
giovannipastorini <giovannipastorini9@gmail.com>
2026-04-02 19:30:13 +0200

    add button and event listener to the button

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
-/* console.log("it works"); */
-
-
 const ulEl=document.getElementById("email-list");
-/* console.log(ulEl); */
+
 
 const api_endpoint='https://flynn.boolean.careers/exercises/api/random/mail';
 
+const btnEl=document.querySelector(".btn");
+
+btnEl.addEventListener("click", function (){
+    //svuoto la lista
+    ulEl.innerHTML="";
+    //utilizzo il metodo nativo ajax: fetch  
+    for (let i=0; i<10; i++){
+    fetch(api_endpoint)
+    .then(response => response.json())
+    .then(data => {
+        // the API response data is inside the parameter
+        console.log(data);
+        const liEl= document.createElement("li");
+        liEl.innerHTML=data.response;
+        ulEl.appendChild(liEl);
+    }) 
+} 
+
+
+})
+
+
+/* 
 //utilizzo il metodo nativo ajax: fetch  
 for (let i=0; i<10; i++){
    fetch(api_endpoint)
@@ -17,5 +37,6 @@ for (let i=0; i<10; i++){
     liEl.innerHTML=data.response;
     ulEl.appendChild(liEl);
   }) 
-}
+} 
+*/
 
